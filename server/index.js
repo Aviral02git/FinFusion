dd9e2f0714b3e1294205befd3909abe5c2a9744f
Aviral Mishra <04aviral.mishra@gmail.com>
2025-12-02 01:28:21 +0530

    new db update

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 const app=express();
 
 app.use(cors({
-    origin: ["https://fin-fusion-wheat.vercel.app", "http://localhost:3000"],
+    origin: ["https://fin-fusion-wheat.vercel.app","*"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   }));
